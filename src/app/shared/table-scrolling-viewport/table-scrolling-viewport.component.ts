10ebe3012141eb4e18808f699367fec2e0d37d3f
Oleksandr <uaman89@gmail.com>
2024-06-07 17:43:19 +0200

    feat: fix sticke header

--- src/app/shared/table-scrolling-viewport/table-scrolling-viewport.component.ts
+++ src/app/shared/table-scrolling-viewport/table-scrolling-viewport.component.ts
@@ -17,12 +17,37 @@ export class TableScrollingViewportComponent implements OnInit, OnChanges {
 
   virtualItems: undefined[] = [];
 
+  offset = '0';
+
   constructor() { }
 
   ngOnInit() {
+    // const el = this.cdkViewport.elementRef.nativeElement;
     this.cdkViewport.renderedRangeStream.subscribe((range: ListRange) => {
+      // this.offset = this.cdkViewport.getOffsetToRenderedContentStart();
+      // // el.style.setProperty('--offset', `-${this.offset}px`);
+      // console.log('set offset:', this.offset);
       this.itemsRangeChange.emit(range);
     });
+
+    // mutation observer
+    const targetNode = this.cdkViewport._contentWrapper.nativeElement;
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver((mutationList) => {
+      for (const mutation of mutationList) {
+        if (mutation.type === "attributes") {
+          console.log(`The ${mutation.attributeName} attribute was modified.`);
+          if (mutation.attributeName === 'style') {
+            const matched = targetNode.style.transform.match(/translateY\((.+)\)/);
+            this.offset = matched[1] || '0';
+            this.cdkViewport.elementRef.nativeElement.style.setProperty('--offset', `-${this.offset}`);
+          }
+        }
+      }
+    });
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, { attributes: true });
+
   }
 
   ngOnChanges(changes: SimpleChanges) {
